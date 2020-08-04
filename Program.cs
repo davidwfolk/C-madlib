34604e8d9f15e7fd2394caf18e3d3734776139bc
David Folk <davidwfolk@gmail.com>
2020-08-04 12:40:39 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace madlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
