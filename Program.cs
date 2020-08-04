4c18d640d851e157548fcfd4d9c29834df0678db
David Folk <davidwfolk@gmail.com>
2020-08-04 13:08:40 -0600

    madlib complete

--- Program.cs
+++ Program.cs
@@ -20,6 +20,17 @@ namespace madlib
             Console.Write("Enter a family member's name: ");
             familyMember = Console.ReadLine();
 
+            Console.Write("\r\n");
+            Console.Write("--------------------------------");
+            Console.Write("\r\n");
+            Console.Write("\r\n");
+            
+            Console.WriteLine("Roses are " + color + " ,");
+            Console.WriteLine(pluralNoun + " are blue,");
+            Console.WriteLine(petName + " is sweet,");
+            Console.WriteLine("But for "+ familyMember + " that's not true.");
+
+            Console.ReadLine();
         }
     }
 }
